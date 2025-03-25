3252b3435f0686a45e22d0bc2c48b5f7986db157
Jaron Wenger <147181586+JaronWenger@users.noreply.github.com>
2025-03-24 19:19:15 -0500

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/whitewatervibes/',
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three', 'three/examples/jsm/objects/Water.js', 'three/examples/jsm/objects/Sky.js']
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      'three': 'three'
-    }
-  },
-  optimizeDeps: {
-    include: ['three', 'three/examples/jsm/objects/Water.js', 'three/examples/jsm/objects/Sky.js']
-  },
-  publicDir: 'public'
+    sourcemap: true
+  }
 }); 
